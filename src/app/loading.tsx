8128b1e48d42041fc37087d4f23131ec411761f3
Cosmic Silence <nsambaevans0@gmail.com>
2025-06-02 00:29:23 +0000

    navigation is still slow at changing pages

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+
+import BrandLoader from '@/components/ui/brand-loader';
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <BrandLoader />;
+}
