70ac09097a6f1343d619c8564d7a2178d8b3c7ee
Cosmic Silence <nsambaevans0@gmail.com>
2025-10-23 11:42:48 +0000

    this section should only contain cutom tour planning content ,so separat

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -184,8 +184,8 @@ export default function PackagesPage() {
                 Plan a Custom Tour
             </Link>
           </Button>
-          <TestimonialCarousel />
       </section>
+      <TestimonialCarousel />
     </div>
   );
 }
