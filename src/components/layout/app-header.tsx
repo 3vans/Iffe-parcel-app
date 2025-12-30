76691211bde5a57f3c70421c7c6c7720eeaf3d61
Cosmic Silence <nsambaevans0@gmail.com>
2025-12-30 07:19:40 +0000

    add a slight magnifing effect to the glass morphsm (_for element <AppHea

--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -92,7 +92,7 @@ const AppHeader = () => {
       <header 
         ref={headerRef}
         className={cn(
-          "bg-card/75 backdrop-blur-lg border-b shadow-lg sticky top-0 z-50 transition-transform duration-300 ease-in-out",
+          "bg-card/75 backdrop-blur-xl border-b shadow-lg sticky top-0 z-50 transition-transform duration-300 ease-in-out",
           showNavbar ? "translate-y-0" : "-translate-y-full"
         )}
       >
