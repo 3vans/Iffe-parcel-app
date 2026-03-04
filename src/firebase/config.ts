2df3901b2b917e247dc687d2510278115015b38c
Cosmic Silence <nsambaevans0@gmail.com>
2026-03-04 10:07:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "e-rotary-hub",
+  "appId": "1:814647349679:web:f91c255e733fa0dd3e5f5a",
+  "apiKey": "AIzaSyBXi1UU4UmbKxCJ8SjtbvRqiUEp0rb-E_M",
+  "authDomain": "e-rotary-hub.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "814647349679"
+};
